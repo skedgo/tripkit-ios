c0e3b95f05ff89467ea9e218de37c1da356dbc30
pnuts2 <brian@skedgo.com>
2016-06-22 12:14:10 +1000

    Fix for repeated server calls when game is added

--- AddOns/Agenda/TKAgendaManager.swift
+++ AddOns/Agenda/TKAgendaManager.swift
@@ -87,11 +87,35 @@ public class TKAgendaManager {
         lastError.value = error
         return Observable.empty().asDriver(onErrorJustReturn: [])
       }
-    }
+      }.distinctUntilChanged { previous, new in
+        return self.outputItemsEqual(previous, new: new)
+      }
     
     let agenda = TKSimpleAgenda(items: trackItems, lastError: lastError.asObservable(), dateComponents: dateComponents)
     agendas[key] = agenda
     return agenda
+  }
+  
+  private func outputItemsEqual(previous: [TKAgendaOutputItem], new: [TKAgendaOutputItem]) -> Bool {
+    if previous.count != new.count {
+      return false
+    }
+    
+    for pair in zip(previous, new) {
+      switch pair {
+      case (.Event, .Event), (.Trip, .Trip), (.TripPlaceholder, .TripPlaceholder):
+        // Treat same base type as the same
+        // TODO: FIX!
+        break
+      case (.TripOptions(let prevItems), .TripOptions(let newItems)) where prevItems.count == newItems.count:
+        // Treat same count as the same
+        break
+      default:
+        return false
+      }
+    }
+    
+    return true
   }
 }
 
