a9a54f2a6ab39e0e0fcfd3be1f2693fc3726ba5e
Adrian Schoenig <adrian.schoenig@gmail.com>
2016-07-19 15:08:40 +1000

    Compile fix. This file is needed for frameworks

---
+++ Classes/TripKit.h
@@ -0,0 +1,14 @@
+//
+//  TripKit.h
+//  Pods
+//
+//  Created by Adrian Schoenig on 22/06/2016.
+//
+//
+
+#ifndef TripKit_h
+#define TripKit_h
+
+// Empty header, required to build framework
+
+#endif /* TripKit_h */
