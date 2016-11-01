6a43feec216fe0c05727e8b23c77f9318846262e
Adrian Schoenig <adrian.schoenig@gmail.com>
2016-11-01 12:24:47 +1100

    Helper method

---
+++ Classes/model/TKSegment.swift
@@ -0,0 +1,28 @@
+//
+//  TKSegment.swift
+//  Pods
+//
+//  Created by Adrian Schoenig on 31/10/16.
+//
+//
+
+import Foundation
+
+extension TKSegment {
+  
+  /// Validates the segment, to make sure it's in a consistent state.
+  /// If it's in an inconsistent state, many things can go wrong. You might
+  /// want to add calls to this method to assertions and precondition checks.
+  public func validate() -> Bool {
+    // Segments need a trip
+    guard let trip = trip else { return false }
+    
+    // A segment should be in its trip's segments
+    guard let _ = trip.segments().index(of: self) else { return false }
+    
+    // Passed all checks
+    return true
+  }
+  
+}
+
