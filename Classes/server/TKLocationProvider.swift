d2338c55a7146a4f9927006a0bb21c9fe4b37bab
Adrian Schoenig <adrian.schoenig@gmail.com>
2017-05-18 16:44:09 +0200

    Use modes from user profile by default

--- Classes/server/TKLocationProvider.swift
+++ Classes/server/TKLocationProvider.swift
@@ -23,7 +23,7 @@ public enum TKLocationProvider {
   /// - Parameters:
   ///   - center: Centre coordinate of circle
   ///   - radius: Radius of circle in metres
-  ///   - modes: Modes for which to fetch locations
+  ///   - modes: Modes for which to fetch locations. If not provided, will use user profile.
   /// - Returns: Observable of fetched locations; can error out
   public static func fetchLocations(center: CLLocationCoordinate2D, radius: CLLocationDistance, modes: [String]? = nil) -> Observable<[STKModeCoordinate]?> {
     
@@ -41,7 +41,13 @@ public enum TKLocationProvider {
       "lng": center.longitude,
       "radius": radius,
     ]
-    paras["modes"] = modes
+    
+    if let modes = modes {
+      paras["modes"] = modes
+    } else {
+      let regionModes = region.modeIdentifiers
+      paras["modes"] = Array(TKUserProfileHelper.maximizedModeIdentifiers(regionModes))
+    }
     
     return SVKServer.sharedInstance().rx
       .hit(.GET, path: "locations.json", parameters: paras, region: region)
