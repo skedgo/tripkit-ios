53dbba7eea1994b10db2df155dc0e891cd79c188
Adrian Schoenig <adrian.schoenig@gmail.com>
2017-08-01 11:27:44 +0200

    Preparing for example

---
+++ Examples/MiniMap/MiniMap/AppDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  AppDelegate.swift
+//  MiniMap
+//
+//  Created by Adrian Schoenig on 7/7/17.
+//  Copyright © 2017 SkedGo Pty Ltd. All rights reserved.
+//
+
+import Cocoa
+
+import TripKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    
+    TripKit.apiKey = "MY_API_KEY"
+    TripKit.prepareForNewSession()
+    
+//    let start: MKAnnotation = ...
+//    let end: MKAnnotation = ...
+//    
+//    let request = TripRequest.insert(
+//      from: start, to: end,
+//      for: nil, timeType: .leaveASAP,
+//      into: TKTripKit.sharedInstance().tripKitContext
+//    )
+//    
+//    let router = TKBuzzRouter()
+//    router.modeIdentifiers = [ SVKTransportModeIdentifierRegularPublicTransport ]
+//    
+//    router.fetchTrips(for: request, success: { request, _ in
+//      
+//      let trips = request.trips()
+//      // show trips
+//      
+//    }, failure: { error, _ in
+//      // handle error
+//    })
+    
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Examples/MiniMap/MiniMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MiniMap
+//
+//  Created by Adrian Schoenig on 7/7/17.
+//  Copyright © 2017 SkedGo Pty Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
