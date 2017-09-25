170f8d7f1942517ff1c630056b2060cd99046fc0
pnuts2 <brian@skedgo.com>
2017-09-25 16:47:39 +1000

    Actually move TKAlertInfoView files to TripKitUI

---
+++ TripKitUI/views/TKAlertInfoView.swift
@@ -0,0 +1,110 @@
+//
+//  TKAlertInfoView.swift
+//  TripGo
+//
+//  Created by Kuan Lun Huang on 23/9/17.
+//  Copyright © 2017 SkedGo Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+import TripKit
+
+public class TKAlertInfoView: UIView {
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var instructionLabel: UILabel!
+  
+  
+  /// Indicating whether the alert info view is currently showing the full
+  /// content or only the title.
+  private(set) var isShowingFullContent = true
+  
+  
+  /// The alert used to configure the alert info view. Specifically, the
+  /// `title` and `text` properties will be used by the titleLabel and
+  /// instructionLabel respectively.
+  ///
+  public var alert: Alert? {
+    didSet {
+      guard let alert = alert else { return }
+      titleLabel.text = alert.title
+      instructionLabel.text = alert.text
+    }
+  }
+  
+  
+  /// Create a new instance of TKAlertInfoView, with title & instruction labels
+  /// set to default texts.
+  ///
+  /// - Returns: An instance of TKAlertInfoView.
+  public class func newInstance() -> TKAlertInfoView {
+    return Bundle.tripKitUI.loadNibNamed("TKAlertInfoView", owner: self, options: nil)!.first as! TKAlertInfoView
+  }
+  
+  
+  /// Create a new instance of TKAlertInfoView based on an alert. That is, the
+  /// title and instruction labels set their `text` property to the alert's 
+  /// title and text properties respectively.
+  ///
+  /// - Parameter alert: An alert instance used to configure title and instruction labels.
+  /// - Returns: An instance of TKAlertInfoView
+  public class func newInstance(with alert: Alert) -> TKAlertInfoView {
+    let view = newInstance()
+    view.alert = alert
+    return view
+  }
+  
+  
+  /// Asks the alert info view to show only its title, i.e., hide the instruction
+  /// portion of the view.
+  public func showTitleOnly() {
+    guard superview != nil, isShowingFullContent else { return }
+    frame.origin.y += frame.size.height - instructionLabel.frame.origin.y
+    isShowingFullContent = false
+  }
+  
+  
+  /// Ask the alert info view to show its entire content, including both title
+  /// and instruction.
+  public func showFullContent() {
+    guard superview != nil, !isShowingFullContent else { return }
+    frame.origin.y -= frame.size.height - instructionLabel.frame.origin.y
+    isShowingFullContent = true
+  }
+  
+  
+  /// Toggles the alert info view betwen showing title only or full content.
+  public func toggle() {
+    isShowingFullContent ? showTitleOnly() : showFullContent()
+  }
+  
+  
+  /// Asks the alert info view to size itself so that it just fits the
+  /// containing view. Note that, this method is usually called before
+  /// the alert view is inserted into the containing view, especially 
+  /// so when the containing view isn't implemented using auto layout.
+  ///
+  /// - Parameter containingView: The view in which the alert info view will appear.
+  public func sizeToFitContent(within containingView: UIView) {
+    guard containingView.frame.width > 0 else {
+      // If the containing view has zero width, a sign that layout is still in
+      // progess, continuing on will produce a bunch of auto layout warnings.
+      return
+    }
+    
+    frame.size.width = containingView.frame.width
+    
+    // Perform a layout pass so that all subviews have the correct frames.
+    setNeedsLayout()
+    layoutIfNeeded()
+    
+    // This is the height that can just fit everything in the view.
+    let fittingSize = systemLayoutSizeFitting(UILayoutFittingCompressedSize)
+    
+    // Update the frame to assume fitting height.
+    frame.size.height = fittingSize.height
+  }
+  
+}
+
