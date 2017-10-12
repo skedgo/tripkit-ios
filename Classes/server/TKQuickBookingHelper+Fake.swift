dcd4016ed178b32b0b09d438a9e22fc317e02bbe
Adrian Schoenig <adrian.schoenig@gmail.com>
2017-10-12 13:01:13 +0200

    Compile fix + a test

--- Classes/server/TKQuickBookingHelper+Fake.swift
+++ Classes/server/TKQuickBookingHelper+Fake.swift
@@ -1,6 +1,6 @@
 //
 //  TKQuickBookingHelper+Fake.swift
-//  Pods
+//  TripKit
 //
 //  Created by Adrian Schoenig on 14/12/16.
 //
@@ -9,59 +9,63 @@
 import Foundation
 
 extension TKBooking.Confirmation {
-  static func fakeTNC() -> TKBooking.Confirmation? {
-    let fake = [
+  static func fakeTNC() -> TKBooking.Confirmation {
+    let fake = """
+    {
       "actions": [
-        [
+        {
           "externalURL": "tel:(555)555-5555",
           "isDestructive": false,
           "title": "Call driver"
-        ],
-        [
+        },
+        {
           "internalURL": "http://deep-thought:8080/satapp-debug/booking/v1/1204f411-eacb-406c-8fd2-3775c8242b02/cancel?bsb=1",
           "isDestructive": true,
           "title": "Cancel ride"
-        ]
+        }
       ],
-      "provider": [
+      "provider": {
         "imageURL": "https://d1a3f4spazzrp4.cloudfront.net/uberex-sandbox/images/driver.jpg",
         "subtitle": "4.9",
         "title": "John"
-      ],
-      "status": [
+      },
+      "status": {
         "title": "Approaching",
         "subtitle": "Your driver is approaching"
-      ],
-      "vehicle": [
+      },
+      "vehicle": {
         "imageURL": "https://d1a3f4spazzrp4.cloudfront.net/uberex-sandbox/images/prius.jpg",
         "subtitle": "UBER-PLATE",
         "title": "Prius Toyota"
-      ],
-      "purchase": [
+      },
+      "purchase": {
         "price": 15.80,
         "currency": "AUD",
         "productName": "uberX",
         "productType": "ps_tnc",
         "id": "1204f411-eacb-406c-8fd2-3775c8242b02",
-      ]
-      ] as [String : Any]
-    return try? TKBooking.Confirmation(object: fake)
+      }
+    }
+    """
+    return try! JSONDecoder().decode(TKBooking.Confirmation.self, from: fake.data(using: .utf8)!)
   }
   
-  static func fakePublic() -> TKBooking.Confirmation? {
-    let fake = [
+  static func fakePublic() -> TKBooking.Confirmation {
+    let fake = """
+    {
       "actions": [
-        [
+        {
           "externalURL": "qrcode:http://www.skedgo.com",
           "isDestructive": false,
           "title": "View ticket"
-        ],
+        },
       ],
-      "status": [
+      "status": {
         "title": "30 Minute Ticket",
         "subtitle": "Valid until 15:30",
-      ],
-      ] as [String : Any]
-    return try? TKBooking.Confirmation(object: fake)
+      },
+    }
+    """
+    return try! JSONDecoder().decode(TKBooking.Confirmation.self, from: fake.data(using: .utf8)!)
   }
 }
