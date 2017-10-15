8ed5cf91dcf9287d6dda39b3058eca00597d35e2
Adrian Schoenig <adrian.schoenig@gmail.com>
2017-10-15 20:14:02 +0200

    Fixing TKShareHelperTest failures

--- Classes/server/TKParserHelper.swift
+++ Classes/server/TKParserHelper.swift
@@ -13,9 +13,14 @@ extension TKParserHelper {
   @objc
   public static func parseDate(_ object: Any?) -> Date? {
     if let string = object as? String {
+      if let interval = TimeInterval(string), interval > 1000000000, interval < 2000000000 {
+        return Date(timeIntervalSince1970: interval)
+      }
       return try? Date(iso8601: string)
+      
     } else if let interval = object as? TimeInterval, interval > 0 {
       return Date(timeIntervalSince1970: interval)
+      
     } else {
       return nil
     }
