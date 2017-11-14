075cb30f49c2ba2a5b53d2a42592a05524170101
Adrian Schoenig <adrian.schoenig@gmail.com>
2017-11-14 08:47:03 +0100

    Updated model for car pods

--- Classes/model/API/LocationAPIModel.swift
+++ Classes/model/API/LocationAPIModel.swift
@@ -55,16 +55,31 @@ extension API {
     
     public let identifier: String
     public let operatorInfo: API.CompanyInfo
-    public let vehicles: [API.CarPodInfo.Vehicle]
+    public let vehicles: [API.CarPodInfo.Vehicle]?
+    public let inService: Bool = true
+
+    public let availableVehicles: Int?
+    public let availableChargingSpaces: Int?
+    public let totalSpaces: Int?
     
     private enum CodingKeys: String, CodingKey {
       case identifier
       case operatorInfo = "operator"
       case vehicles
+      case availableVehicles
+      case availableChargingSpaces
+      case totalSpaces
     }
     
+    public var availableSpaces: Int? {
+      guard let total = totalSpaces, let vehicles = availableVehicles else { return nil }
+      
+      // available vehicles can exceed number of spaces!
+      return max(0, total - vehicles)
+    }
+
     public var hasRealTime: Bool {
-      return false // not yet
+      return inService && availableVehicles != nil
     }
   }
   
