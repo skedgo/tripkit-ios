fbd04da4fd82c85e58bbd8f68608521182ab77d6
Adrian Schoenig <adrian.schoenig@gmail.com>
2018-02-08 22:36:02 +0100

    Fix missing imports

--- TripKit-macOS/TripKit_macOS.h
+++ TripKit-macOS/TripKit_macOS.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char TripKit_macOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TripKit_macOS/PublicHeader.h>
 
+#import "TKTripKit.h"
 
--- TripKitInterApp-iOS/TripKitInterApp.h
+++ TripKitInterApp-iOS/TripKitInterApp.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char TripKitInterApp_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TripKitInterApp_iOS/PublicHeader.h>
 
-
+#import "TKInterAppCommunicator.h"
