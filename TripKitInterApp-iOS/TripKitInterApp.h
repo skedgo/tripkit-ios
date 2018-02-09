7f0796549d711eb8541d6df0159e4edd85c01bf5
Adrian Schoenig <adrian.schoenig@gmail.com>
2018-02-09 11:46:05 +0100

    Regression fixes

--- TripKitInterApp-iOS/TripKitInterApp.h
+++ TripKitInterApp-iOS/TripKitInterApp.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char TripKitInterApp_iOSVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <TripKitInterApp_iOS/PublicHeader.h>
 
 #import "TKInterAppCommunicator.h"
+#import "SGKConfig+TKInterAppCommunicator.h"
