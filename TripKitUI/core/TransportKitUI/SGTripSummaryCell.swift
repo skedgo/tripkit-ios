19c3d3ed89604921fb4d8e5eef96b5f24268339b
Brian Huang <brian@skedgo.com>
2018-03-19 17:14:00 +1100

    Provided an option to produce low-constrast trip segment view (#61)
    * Provided an option to produce low-constrast trip segment view
    
    * darkGreyColor -> darkTextColor, lightGreyColor -> lightTextColor

--- TripKitUI/core/TransportKitUI/SGTripSummaryCell.swift
+++ TripKitUI/core/TransportKitUI/SGTripSummaryCell.swift
@@ -100,6 +100,8 @@ extension SGTripSummaryCell {
   private func updateSegments(nano: Bool) {
     segmentView?.allowWheelchairIcon = allowWheelchairIcon
     segmentView?.colorCodingTransitIcon = colorCodingTransitIcon
+    segmentView?.darkTextColor = darkTextColor
+    segmentView?.lightTextColor = lightTextColor
     segmentView?.configure(forSegments: _trip.segments(with: .inSummary), allowSubtitles: !nano, allowInfoIcons: !nano)
   }
   
