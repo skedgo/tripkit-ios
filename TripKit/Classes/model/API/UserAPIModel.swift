f76ab6181e9ff1c48230af418ccaf739f1b874b3
Brian Huang <brian@skedgo.com>
2018-04-27 00:17:41 +1000

    Refining Phone & Email Codable + appData = [String : Any]

--- TripKit/Classes/model/API/UserAPIModel.swift
+++ TripKit/Classes/model/API/UserAPIModel.swift
@@ -11,15 +11,15 @@ import Foundation
 extension API {
   
   public struct User: Codable {
-    public let givenName: String?
-    public let surname: String?
+    public let firstName: String?
+    public let lastName: String?
     public let address1: String?
     public let address2: String?
     public let postCode: String?
     public let emails: [Email]?
     public let phones: [Phone]?
     public let userId: String?
-    public let appData: [String : String]? // TODO: Can we make this more generic?
+    public var appData: [String : Any] = [:]
     
     public init(
       firstName: String? = nil,
@@ -30,10 +30,10 @@ extension API {
       emails: [Email]? = nil,
       phones: [Phone]? = nil,
       userId: String? = nil,
-      appData: [String : String]? = nil
+      appData: [String : Any] = [:]
       ) {
-      self.givenName = firstName
-      self.surname = lastName
+      self.firstName = firstName
+      self.lastName = lastName
       self.address1 = address1
       self.address2 = address2
       self.postCode = postCode
@@ -42,39 +42,70 @@ extension API {
       self.userId = userId
       self.appData = appData
     }
+    
+    // MARK: - Codable
+    
+    private enum CodingKeys: String, CodingKey {
+      case firstName = "givenName"
+      case lastName = "surname"
+      case address1
+      case address2
+      case postCode
+      case emails
+      case phones
+      case userId
+    }
   }
   
   public struct Phone: Codable {
-    public let phoneCode: String?
-    public let phone: String
+    public let countryCode: String?
+    public let number: String
     let validated: Bool?
     public let type: String?
     public let id: String?
     
     public init(number: String, countryCode: String? = nil, validated: Bool = false, type: String? = nil, id: String? = nil) {
-      self.phoneCode = countryCode
-      self.phone = number
+      self.countryCode = countryCode
+      self.number = number
       self.validated = validated
       self.type = type
       self.id = id
     }
     
     var isValidated: Bool { return validated ?? false }
+    
+    // MARK: - Codable
+    
+    private enum CodingKeys: String, CodingKey {
+      case countryCode = "phoneCode"
+      case number = "phone"
+      case validated
+      case type
+      case id
+    }
   }
   
   public struct Email: Codable {
-    public let email: String
+    public let address: String
     let validated: Bool?
     let primary: Bool?
     
     public init(address: String, validated: Bool = false, primary: Bool = false) {
-      self.email = address
+      self.address = address
       self.validated = validated
       self.primary = primary
     }
     
     var isValidated: Bool { return validated ?? false }
     var isPrimary: Bool { return primary ?? false }
+    
+    // MARK: - Codable
+    
+    private enum CodingKeys: String, CodingKey {
+      case address = "email"
+      case validated
+      case primary
+    }
   }
   
 }
