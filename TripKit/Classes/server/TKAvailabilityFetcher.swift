9914df1df788b971e4549e2ecadc72447d68b26b
Adrian Schoenig <adrian.schoenig@gmail.com>
2018-11-09 22:47:44 +0100

    Clean-up

--- TripKit/Classes/server/TKAvailabilityFetcher.swift
+++ TripKit/Classes/server/TKAvailabilityFetcher.swift
@@ -13,9 +13,6 @@ import RxSwift
 extension TKBuzzInfoProvider {
   @available(iOS 10.0, *)
   public static func fetchVehicleAvailabilities(locationId: String, in region: TKRegion) -> Single<[API.CarAvailability]> {
-    
-    //    return .just(TKBuzzInfoProvider.AvailabilityResponse.fake.cars)
-    
     let paras: [String: Any] = [ "identifier": locationId, "region": region.name ]
     return TKServer.shared.rx
       .hit(.GET, path: "locationInfo.json", parameters: paras, region: region)
@@ -28,79 +25,4 @@ extension TKBuzzInfoProvider {
         return info.carPod?.availabilities ?? []
       }
   }
-}
-
-fileprivate extension TKBuzzInfoProvider {
-  struct AvailabilityResponse: Codable {
-    let cars: [API.CarAvailability]
-  }
-}
-
-extension TKBuzzInfoProvider.AvailabilityResponse {
-  fileprivate static var fake: TKBuzzInfoProvider.AvailabilityResponse = {
-    let now = Date().timeIntervalSince1970
-    let end1 = now  + TimeInterval.random(in: (1...5)) * 3600
-    let end2 = end1 + TimeInterval.random(in: (3...10)) * 3600
-
-    let fake = """
-    {
-      "cars": [
-        {
-          "car": {
-            "identifier": "CAR1",
-            "name": "Moritz",
-            "description": "Renault Zoe"
-          },
-          "availability": {
-            "lastUpdated": \(now),
-            "intervals": [
-              {
-                "end": \(end1),
-                "status": "AVAILABLE"
-              },
-              {
-                "start": \(end1),
-                "end": \(end2),
-                "status": "NOT_AVAILABLE"
-              },
-              {
-                "start": \(end2),
-                "status": "AVAILABLE"
-              },
-            ]
-          }
-        },
-        {
-          "car": {
-            "identifier": "CAR2",
-            "name": "Franz",
-            "description": "Nissan Leaf"
-          },
-          "availability": {
-            "lastUpdated": \(now),
-            "intervals": [
-              {
-                "end": \(end1),
-                "status": "NOT_AVAILABLE"
-              },
-              {
-                "start": \(end1),
-                "end": \(end2),
-                "status": "AVAILABLE"
-              },
-              {
-                "start": \(end2),
-                "status": "UNKNOWN"
-              },
-            ]
-          }
-        },        
-      ]
-    }
-    """
-    let data = fake.data(using: .utf8)!
-    let decoder = JSONDecoder()
-    decoder.dateDecodingStrategy = .secondsSince1970
-    return try! decoder.decode(TKBuzzInfoProvider.AvailabilityResponse.self, from: data)
-  }()
 }
