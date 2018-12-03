688d76ef2f4ecd3d407e0522c1408cecc0cb5694
Adrian Schoenig <adrian.schoenig@gmail.com>
2018-12-03 15:41:42 +0100

    Allow passing date

--- TripKit/Classes/server/TKAvailabilityFetcher.swift
+++ TripKit/Classes/server/TKAvailabilityFetcher.swift
@@ -11,9 +11,18 @@ import Foundation
 import RxSwift
 
 extension TKBuzzInfoProvider {
+  
   @available(iOS 10.0, *)
-  public static func fetchVehicleAvailabilities(locationId: String, in region: TKRegion) -> Single<[API.CarAvailability]> {
-    let paras: [String: Any] = [ "identifier": locationId, "region": region.name ]
+  public static func fetchVehicleAvailabilities(locationId: String, in region: TKRegion, start: Date? = nil, end: Date? = nil) -> Single<[API.CarAvailability]> {
+    
+    var paras: [String: Any] = [ "identifier": locationId, "region": region.name ]
+    if let start = start {
+      paras["start"] = ISO8601DateFormatter().string(from: start)
+    }
+    if let end = end {
+      paras["end"] = ISO8601DateFormatter().string(from: end)
+    }
+    
     return TKServer.shared.rx
       .hit(.GET, path: "locationInfo.json", parameters: paras, region: region)
       .asSingle()
@@ -25,4 +34,18 @@ extension TKBuzzInfoProvider {
         return info.carPod?.availabilities ?? []
       }
   }
+  
+  @available(iOS 10.0, *)
+  public static func fetchVehicleAvailabilities(locationId: String, in region: TKRegion, filter: Observable<(start: Date, end: Date)?>) -> Observable<[API.CarAvailability]> {
+    
+    // TODO: We could be smarter about this, cache the previous result and only
+    //   query again, if we need new data.
+    
+    return filter
+      .flatMapLatest { tuple in
+        return fetchVehicleAvailabilities(locationId: locationId, in: region, start: tuple?.start, end: tuple?.end)
+      }
+    
+  }
+  
 }
