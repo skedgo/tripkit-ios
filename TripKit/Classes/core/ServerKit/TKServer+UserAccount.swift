dc3a6e5c8a59e5842b03543999cb2e6f5dd27805
Adrian Schoenig <adrian.schoenig@gmail.com>
2019-01-31 10:56:33 +0100

    Supporting new /auth/uuid endpoint

--- TripKit/Classes/core/ServerKit/TKServer+UserAccount.swift
+++ TripKit/Classes/core/ServerKit/TKServer+UserAccount.swift
@@ -25,17 +25,37 @@ extension Reactive where Base == TKServer {
   ///
   /// - Parameter cloudKitID: The record name of the CloudKit's
   ///     user record ID.
-  /// - Returns: The user token
+  /// - Returns: The user token. Can fail.
   public func signIn(withCloudKitID cloudKitID: String) -> Single<String> {
-    
-    let urlFriendly = cloudKitID.replacingOccurrences(of: "_", with: "")
-    return hit(.POST, path: "account/apple/\(urlFriendly)")
+    return signIn(token: cloudKitID, endpoint: "apple")
+  }
+  
+  /// Sign the user in using a UUID, which you might sync using whichever means
+  /// you prefer.
+  ///
+  /// - Note: The token is only retrieved, but not stored. Most
+  ///     likely you'll next want to call
+  ///     `TKServer.updateUserToken(_:)`.
+  ///
+  /// - Note: There's no need to sign-up first. If this is the
+  ///     sign-in attempt an account with no information but the
+  ///     UUID will be created.
+  ///
+  /// - Parameter uuid: A UUID of your choosing
+  /// - Returns: The user token. Can fail.
+  public func signIn(withUUID uuid: String) -> Single<String> {
+    return signIn(token: uuid, endpoint: "uuid")
+  }
+  
+  private func signIn(token: String, endpoint: String) -> Single<String> {
+    let urlFriendly = token.replacingOccurrences(of: "_", with: "")
+    return hit(.POST, path: "account/\(endpoint)/\(urlFriendly)")
       .map { _, _, data in
         guard let data = data, let response = try? JSONDecoder().decode(SignInResponse.self, from: data) else {
           throw TKError.error(withCode: 1301, userInfo: [ NSLocalizedDescriptionKey: "Cannot find a valid token" ])
         }
         return response.userToken
-      }
+    }
   }
   
 }
