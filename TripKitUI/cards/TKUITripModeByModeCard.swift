c32e9583f0c4e9819009655ea177babd4ddba21c
Adrian Schoenig <adrian.schoenig@gmail.com>
2019-03-13 08:30:46 +0100

    Fixes issue where tapping a segment might show the wrong card

--- TripKitUI/cards/TKUITripModeByModeCard.swift
+++ TripKitUI/cards/TKUITripModeByModeCard.swift
@@ -16,6 +16,27 @@ public class TKUITripModeByModeCard: TGPageCard {
     case segmentTripDoesNotMatchMapManager
   }
   
+  /// Storage of information of what the cards are used for the segment at a
+  /// specific index. There is one of these for every index, but not all are
+  /// guarantueed to have cards, i.e., `cards` can be empty.
+  struct SegmentCards {
+    let segmentIndex: Int
+    let cards: [TGCard]
+    
+    static func firstCardIndex(ofSegmentAt needle: Int, in haystack: [SegmentCards]) -> Int? {
+      let index: (Int, Int?) = haystack.reduce( (0, nil) ) { acc, card in
+        if acc.1 != nil {
+          return acc
+        } else if card.segmentIndex == needle {
+          return (0, acc.0)
+        } else {
+          return (acc.0 + card.cards.count, nil)
+        }
+      }
+      return index.1
+    }
+  }
+  
   public static var config = Configuration.empty
 
   /// Constructs a page card configured for displaying the segments on a
@@ -28,14 +49,17 @@ public class TKUITripModeByModeCard: TGPageCard {
     }
     
     let segments = segment.trip.segments
-
-    // TODO: This doesn't work if the builder doesn't use exactly one card
-    //   per segment. Instead, we should ask the builder for all the cards,
-    //   and to, optionally, also return the index of the start segment.
-    guard let index = segments.index(of: segment) else { preconditionFailure() }
-    let cards = segments.flatMap { TKUITripModeByModeCard.config.builder.cards(for: $0, mapManager: mapManager) }
+    guard let segmentIndex = segments.index(of: segment) else { preconditionFailure() }
     
-    super.init(cards: cards, initialPage: index)
+    let segmentCards: [SegmentCards] = segments.enumerated().map {
+      let cards = TKUITripModeByModeCard.config.builder.cards(for: $0.element, mapManager: mapManager)
+      return SegmentCards(segmentIndex: $0.offset, cards: cards)
+    }
+    
+    let cards = segmentCards.flatMap { $0.cards }
+    let initialPage = SegmentCards.firstCardIndex(ofSegmentAt: segmentIndex, in: segmentCards)
+    
+    super.init(cards: cards, initialPage: initialPage ?? 0)
   }
   
   public convenience init(mapManager: TKUITripMapManager) {
