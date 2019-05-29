c2a6fb965d8426c6a36f99d73ff4d0d80153bcba
Adrian Schoenig <adrian.schoenig@gmail.com>
2019-05-29 10:32:39 +0000

    Fixes to revive the MiniMap example
    - Update to Swift 5
    - Fix up TripKit's podspecs to point at correct dependencies
    - Fix compile regression when using CocoaPods
    - Add README\

---
+++ TripKitUI/controller/TKUIDeparturesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TKUIDeparturesViewController.swift
+//  TripKitUI-iOS
+//
+//  Created by Adrian Schönig on 24.05.19.
+//  Copyright © 2019 SkedGo Pty Ltd. All rights reserved.
+//
+
+import Foundation
