311eb2cd0682028bff78a7661f5c4dc3e56c3cb3
Adrian Schoenig <adrian.schoenig@gmail.com>
2019-06-22 09:11:39 +0200

    Trying to reproduce crash in polyline renderer (RM11524)

--- TripKitUI/views/TKUIPolylineRenderer.swift
+++ TripKitUI/views/TKUIPolylineRenderer.swift
@@ -81,7 +81,6 @@ open class TKUIPolylineRenderer: MKPolylineRenderer {
   }
   
   private func drawLine(color: CGColor, width: CGFloat, allowDashes: Bool, zoomScale: MKZoomScale, in context: CGContext) {
-    context.addPath(path)
     
     if allowDashes {
       // Defaults take care of dash pattern
@@ -94,7 +93,9 @@ open class TKUIPolylineRenderer: MKPolylineRenderer {
     
     context.setStrokeColor(color)
     context.setLineWidth(width / zoomScale)
-    context.strokePath()
+
+    strokePath(path, in: context)
+
   }
   
   override open func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
