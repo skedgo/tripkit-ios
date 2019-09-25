52f418b549be8b0e858241e31e0388dc960114ba
Brian Huang <brian@skedgo.com>
2019-09-25 18:25:52 +1000

    parking vacancy can be optional

--- TripKit/Classes/model/API/LocationAPIModel.swift
+++ TripKit/Classes/model/API/LocationAPIModel.swift
@@ -262,7 +262,7 @@ extension API {
     public let availableSpaces: Int?
     public let totalSpaces: Int?
     public let occupiedSpaces: Int?
-    public let parkingVacancy: Vacancy
+    public let parkingVacancy: Vacancy?
     public let lastUpdate: TimeInterval?
 
     /// The polyline defining the parking area along the street as an encoded polyline.
