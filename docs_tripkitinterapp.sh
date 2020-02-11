b430366542cf5f4a051d6130679c2b3fdc8b28f1
Adrian Schoenig <adrian@schoenig.me>
2020-02-10 22:37:04 -0300

    Fix typo

--- docs_tripkitinterapp.sh
+++ docs_tripkitinterapp.sh
@@ -2,7 +2,7 @@
 # Bash script to update documentation
 
 (
-  sourcekitten doc -- -project TripKit.xcodeproj -scheme TripKitInterApp-iOS > jazzy-temp.TKUIInterApp.json
+  sourcekitten doc -- -project TripKit.xcodeproj -scheme TripKitInterApp-iOS > jazzy-temp.TKInterApp.json
 
-  jazzy --sourcekitten-sourcefile jazzy-temp.TKUIInterApp.json --config docs/.tripkitinterapp.jazzy.yaml
+  jazzy --sourcekitten-sourcefile jazzy-temp.TKInterApp.json --config docs/.tripkitinterapp.jazzy.yaml
 )
