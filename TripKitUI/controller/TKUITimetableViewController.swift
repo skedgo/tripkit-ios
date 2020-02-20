f78676baa5f013ed6c1e2ddada746e3fc8704a16
Brian <brian@skedgo.com>
2020-02-20 13:52:22 +1100

    support filtering in timetable card
    Part of https://redmine.buzzhives.com/issues/12334

--- TripKitUI/controller/TKUITimetableViewController.swift
+++ TripKitUI/controller/TKUITimetableViewController.swift
@@ -17,6 +17,14 @@ public typealias TKUIDeparturesViewController = TKUITimetableViewController
 public typealias TKUIDeparturesViewControllerDelegate = TKUITimetableViewControllerDelegate
 
 public protocol TKUITimetableViewControllerDelegate: TGCardViewControllerDelegate {
+  
+  /// A callback used to notify that the filtering string has changed in the searh bar.
+  ///
+  /// - Parameters:
+  ///   - controller: The timetable view controller within which the filtering takes place
+  ///   - filter: The latest filtering string
+  func timetableViewController(_ controller: TKUITimetableViewController, updatedFilter filter: String)
+  
 }
 
 /// The `TKUITimetableViewController` class provides a user interface for
@@ -31,20 +39,29 @@ public protocol TKUITimetableViewControllerDelegate: TGCardViewControllerDelegat
 /// - `TKUITimetableCard.config` for the list of departures
 public class TKUITimetableViewController: TGCardViewController {
   
+  public var timetableDelegate: TKUITimetableViewControllerDelegate?
+  
   /// Configure for showing the departures from a public transport stop.
   ///
   /// Make sure that the stop provides the correct region and stop code, or
   /// otherwise you'll get a blank view. Best to use stop annotations provided
   /// from TripKit, e.g., from `TKSkedGoGeocoder`.
   ///
-  /// - Parameter stop: The stop for which to show departures, includes
-  ///     departures from any child stops (e.g., platforms)
-  public init(stop: TKUIStopAnnotation) {
+  /// - Parameters:
+  ///   - stop: The stop for which to show departures, includes departures
+  ///   from any child stops (e.g., platforms)
+  ///   - filter: An optional string used to filter the resulting departures
+  public init(stop: TKUIStopAnnotation, filter: String? = nil) {
     super.init(nibName: "TGCardViewController", bundle: Bundle(for: TGCardViewController.self))
     
-    let departuresCard = TKUITimetableCard(stops: [stop])
-    departuresCard.style = TKUICustomization.shared.cardStyle
-    rootCard = departuresCard
+    let timetableCard = TKUITimetableCard(stops: [stop])
+    timetableCard.style = TKUICustomization.shared.cardStyle
+    timetableCard.filter = filter
+    timetableCard.filterUpdatedHandler = { [weak self] filter in
+      guard let self = self else { return }
+      self.timetableDelegate?.timetableViewController(self, updatedFilter: filter)
+    }
+    rootCard = timetableCard
   }
   
   /// Configure for showing the connections between two public transport stops.
@@ -56,9 +73,9 @@ public class TKUITimetableViewController: TGCardViewController {
   public init(dlsTable: TKDLSTable, startDate: Date = Date()) {
     super.init(nibName: "TGCardViewController", bundle: Bundle(for: TGCardViewController.self))
     
-    let departuresCard = TKUITimetableCard(dlsTable: dlsTable, startDate: startDate)
-    departuresCard.style = TKUICustomization.shared.cardStyle
-    rootCard = departuresCard
+    let timetableCard = TKUITimetableCard(dlsTable: dlsTable, startDate: startDate)
+    timetableCard.style = TKUICustomization.shared.cardStyle
+    rootCard = timetableCard
   }
   
   required public init(coder aDecoder: NSCoder) {
