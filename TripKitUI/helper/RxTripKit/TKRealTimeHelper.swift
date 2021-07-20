ac35342d8185a387187d7eeb5fca767c5c286fbe
Brian Huang <brian@skedgo.com>
2021-07-20 02:59:36 +0000

    Supporting active trip in home card

---
+++ TripKitUI/helper/RxTripKit/TKRealTimeHelper.swift
@@ -0,0 +1,36 @@
+//
+//  TKRealTimeHelper.swift
+//  TripKitUI-iOS
+//
+//  Created by Brian Huang on 2/7/21.
+//  Copyright © 2021 SkedGo Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
+import TripKit
+
+import RxSwift
+
+public enum TKRealTimeHelper {
+  
+  public static func streamRealTime(for trip: Trip, pause: Observable<Bool>) -> Observable<TKRealTimeUpdateProgress<Trip>> {
+    guard trip.wantsRealTimeUpdates else { return .empty() }
+    
+    // LATER: It might be better to only advance the counter once the inner observable has finished. Currently it's up to the builder to not hit the server too frequently.
+    
+    let counterAdvanced = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+    
+    return Observable.combineLatest(counterAdvanced, pause.startWith(false)) { (counter: $0, pause: $1) }
+      .filter { TKUITripModeByModeCard.config.builder.shouldUpdate(trip: trip, counter: $0.counter) && !$0.pause }
+      .flatMapLatest { _ -> Observable<TKRealTimeUpdateProgress<Trip>> in
+        let previousURLString = trip.updateURLString
+        return TKBuzzRealTime.rx.update(trip)
+          .map { ($1 && $0.updateURLString == previousURLString) ? .idle : .updated($0) }
+          .asObservable()
+          .startWith(.updating)
+      }
+      .startWith(.idle)
+  }
+  
+}
--- TripKitUI/view model/TKUITripModeByModeViewModel.swift
+++ TripKitUI/view model/TKUITripModeByModeViewModel.swift
@@ -36,22 +36,7 @@ class TKUITripModeByModeViewModel {
   let realTimeUpdate: Driver<TKRealTimeUpdateProgress<Trip>>
   
   private static func fetchRealTime(for trip: Trip) -> Driver<TKRealTimeUpdateProgress<Trip>> {
-    guard trip.wantsRealTimeUpdates else { return .empty() }
-    
-    // LATER: It might be better to only advance the counter once the inner observable has finished. Currently it's up to the builder to not hit the server too frequently.
-    
-    return Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
-      .filter {
-        TKUITripModeByModeCard.config.builder.shouldUpdate(trip: trip, counter: $0)
-      }
-      .flatMapLatest { (interval) -> Observable<TKRealTimeUpdateProgress<Trip>> in
-        let previousURLString = trip.updateURLString
-        return TKBuzzRealTime.rx.update(trip)
-          .map { ($1 && $0.updateURLString == previousURLString) ? .idle : .updated($0) }
-          .asObservable()
-          .startWith(.updating)
-      }
-      .startWith(.idle)
+    return TKRealTimeHelper.streamRealTime(for: trip, pause: .just(false))
       .asDriver(onErrorDriveWith: .empty())
   }
   
