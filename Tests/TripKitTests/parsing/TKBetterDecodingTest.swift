dc46f90a3cce4135302ef1bf2f3301a2f1f94478
Adrian Schoenig <adrian@schoenig.me>
2021-08-18 15:31:52 +1000

    A decoding test

--- Tests/TripKitTests/parsing/TKBetterDecodingTest.swift
+++ Tests/TripKitTests/parsing/TKBetterDecodingTest.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2021 SkedGo Pty Ltd. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import TripKit
+
+class TKBetterDecodingTest: XCTestCase {
+  
+  struct BillingCycle: Codable {
+    @OptionalISO8601OrSecondsSince1970 var toBeAppliedTimestamp: Date?
+  }
+  
+  func testDecodeOptionalISO() throws {
+    let json = """
+        {
+            "paymentTimestamp": "2021-08-07T18:13:54+10:00[Australia/Brisbane]",
+        }
+        """
+    
+    let model = try JSONDecoder().decode(BillingCycle.self, from: Data(json.utf8))
+    XCTAssertNotNil(model)
+    XCTAssertNil(model.toBeAppliedTimestamp)
+  }
+}
