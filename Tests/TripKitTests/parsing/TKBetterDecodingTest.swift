30d6b6c48af7c88ffaf4c18ae23e229b24fb46a8
Adrian Schoenig <adrian@schoenig.me>
2021-08-18 15:31:43 +1000

    Critical fix for using codable property wrappers from outside

---
+++ Tests/TripKitTests/parsing/TKBetterDecodingTest.swift
@@ -0,0 +1,9 @@
+//
+//  TKBetterDecodingTest.swift
+//  TripKitTests
+//
+//  Created by Adrian Schönig on 18/8/21.
+//  Copyright © 2021 SkedGo Pty Ltd. All rights reserved.
+//
+
+import Foundation
