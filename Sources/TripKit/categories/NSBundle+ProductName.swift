edf32be7155c08061c94d4b230d2f2f48f7487fa
Adrian Schoenig <adrian@schoenig.me>
2022-04-13 07:41:12 +0930

    Bundle.productName should prefer display name

--- Sources/TripKit/categories/NSBundle+ProductName.swift
+++ Sources/TripKit/categories/NSBundle+ProductName.swift
@@ -10,25 +10,19 @@ import Foundation
 
 extension Bundle {
   
+  /// Product name, preferring name from localized Info.plist and preferring display name over product name.
   @objc public var productName: String? {
-    
-    if let localizedDict = localizedInfoDictionary {
-      if let candidate = localizedDict[kCFBundleNameKey as String] as? String, !candidate.hasPrefix("$") {
-        return candidate
-      } else if let candidate = localizedDict["CFBundleDisplayName"] as? String, !candidate.hasPrefix("$") {
-        return candidate
-      }
+    if let candidate = localizedInfoDictionary?["CFBundleDisplayName"] as? String, !candidate.hasPrefix("$") {
+      return candidate
+    } else if let candidate = localizedInfoDictionary?[kCFBundleNameKey as String] as? String, !candidate.hasPrefix("$") {
+      return candidate
+    } else if let candidate = infoDictionary?["CFBundleDisplayName"] as? String, !candidate.hasPrefix("$") {
+      return candidate
+    } else if let candidate = infoDictionary?[kCFBundleNameKey as String] as? String, !candidate.hasPrefix("$") {
+      return candidate
+    } else {
+      return nil
     }
-
-    if let infoDict = infoDictionary {
-      if let candidate = infoDict[kCFBundleNameKey as String] as? String, !candidate.hasPrefix("$") {
-        return candidate
-      } else if let candidate = infoDict["CFBundleDisplayName"] as? String, !candidate.hasPrefix("$") {
-        return candidate
-      }
-    }
-    
-    return nil
   }
   
 }
