c1a7e7a38eb44b95139a212ce0172b422b90bceb
Adrian Schoenig <adrian@schoenig.me>
2023-12-18 18:01:47 +1100

    Updating TKServer.upload

--- Sources/TripKit/server/TKServer+ImageUpload.swift
+++ Sources/TripKit/server/TKServer+ImageUpload.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension TKServer {
   
-  public func upload(_ imageData: Data, onComplete handler: @escaping (Result<TKImage?, Error>) -> Void) {
+  public func upload(imageData: Data, contentType: String) async throws {
     let baseURL = baseURLs(for: nil).first!
     
     let boundary = UUID().uuidString
@@ -21,27 +21,39 @@ extension TKServer {
     request.setValue(userToken, forHTTPHeaderField: "userToken")
     request.setValue(apiKey, forHTTPHeaderField: "X-TripGo-Key")
     request.setValue(TKServer.xTripGoVersion(), forHTTPHeaderField: "X-TripGo-Version")
+    if let headers = TKServer.shared.customHeaders {
+      for (header, value) in headers {
+        request.setValue(value, forHTTPHeaderField: header)
+      }
+    }
     
-    var data = Data()
-    data.append(Data("\r\n--\(boundary)\r\n".utf8))
-    data.append(Data("Content-Disposition: form-data; filename=\"\("user-profile-picture")\"\r\n".utf8))
-    data.append(Data("Content-Type: image/png\r\n\r\n".utf8))
-    data.append(imageData)
-    data.append(Data("\r\n--\(boundary)--\r\n".utf8))
+    var uploadData = Data()
+    uploadData.append(Data("\r\n--\(boundary)\r\n".utf8))
+    uploadData.append(Data("Content-Disposition: form-data; filename=\"\("user-profile-picture")\"\r\n".utf8))
+    uploadData.append(Data("Content-Type: \(contentType)\r\n\r\n".utf8))
+    uploadData.append(imageData)
+    uploadData.append(Data("\r\n--\(boundary)--\r\n".utf8))
     
     let id = UUID()
     TKLog.log("TKServer+Image", request: request, uuid: id)
     
-    let task = URLSession.shared.uploadTask(with: request, from: data) { data, response, error in
-      TKLog.log("TKServer+Image", response: response, data: data, orError: error as NSError?, for: request, uuid: id)
-      if let error = error {
-        handler(.failure(error))
-      } else {
-        handler(.success(TKImage(data: imageData)))
+    do {
+      let (data, response) = try await URLSession.shared.upload(for: request, from: uploadData)
+      TKLog.log("TKServer+Image", response: response, data: data, orError: nil, for: request, uuid: id)
+      if let error = TKError.error(from: data, domain: "com.skedgo.TripKit") {
+        throw error // To be consistent with what `TKServer` does
       }
+    } catch let error as TKError {
+      throw error // Don't log again
+    } catch {
+      TKLog.log("TKServer+Image", response: nil, data: nil, orError: error, for: request, uuid: id)
+      throw error
     }
-    
-    task.resume()
   }
   
+  @discardableResult
+  public func removeImage() async -> Int? {
+    await TKServer.shared.hit(.DELETE, path: "/data/user/image").statusCode
+  }
+
 }
