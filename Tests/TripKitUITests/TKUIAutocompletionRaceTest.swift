0eb414f9a55bb78bcd81470668ab9983655b3b0e
Adrian Schoenig <adrian@schoenig.me>
2024-07-28 22:09:53 +1000

    Revert "Feature/21349 autocomplete merging (#359)"
    This reverts commit a207df927d582b09b5203aa546eba7b9b9aacf8c.

--- Tests/TripKitUITests/TKUIAutocompletionRaceTest.swift
+++ Tests/TripKitUITests/TKUIAutocompletionRaceTest.swift
@@ -11,7 +11,6 @@ import XCTest
 import RxSwift
 import RxCocoa
 
-@testable import TripKit
 @testable import TripKitUI
 
 fileprivate let 💥 = [-1]
@@ -132,31 +131,6 @@ class TKUIAutocompletionRaceTest: XCTestCase {
       .completed(6),
     ]))
   }
-  
-  func testStableRace() throws {
-    let inputs: [([TKAutocompletionResult], at: TestTime)] = [
-      ([TKAutocompletionResult(object: "1", title: "Campbell High School", image: .iconAlert, score: 50)], at: 100),
-      ([TKAutocompletionResult(object: "2", title: "Highway 17", image: .iconAlert, score: 5)], at: 200),
-      ([TKAutocompletionResult(object: "3", title: "Campbell High School", image: .iconAlert, score: 45)], at: 300),
-      ([TKAutocompletionResult(object: "4", title: "Campbell High School Athletics", image: .iconAlert, score: 40)], at: 400),
-      ([TKAutocompletionResult(object: "5", title: "Campbell High School", image: .iconAlert, score: 35)], at: 500),
-      ([TKAutocompletionResult(object: "6", title: "Campbell HS - Basketball Courts", image: .iconAlert, score: 30)], at: 600)
-    ]
-    
-    let expected: [Recorded<Event<[TKAutocompletionResult]>>] = [
-      .next(600, [
-        TKAutocompletionResult(object: "5", title: "Campbell High School", image: .iconAlert, score: 50),
-        TKAutocompletionResult(object: "4", title: "Campbell High School Athletics", image: .iconAlert, score: 40),
-        TKAutocompletionResult(object: "6", title: "Campbell HS - Basketball Courts", image: .iconAlert, score: 30),
-        TKAutocompletionResult(object: "2", title: "Highway 17", image: .iconAlert, score: 5)
-      ]),
-      .completed(600)
-    ]
-    
-    let simulated = runAutoCompleteSimulation(cutOff: 5, fastSpots: 2, inputs)
-            
-    XCTAssertEqual(simulated, expected)
-  }
 
 }
 
@@ -190,8 +164,7 @@ extension TKUIAutocompletionRaceTest {
         .map(scheduler.createHotObservable)
         .map { $0.asObservable() }
       
-      // Threshold set to nil to ignore Levenshtein distance handling for this simulation
-      let processed = Observable.stableRace(observables, cutOff: .seconds(cutOff), fastSpots: fastSpots, threshold: nil)
+      let processed = Observable.stableRace(observables, cutOff: .seconds(cutOff), fastSpots: fastSpots)
       
       processed
         .bind(to: observer)
@@ -203,41 +176,4 @@ extension TKUIAutocompletionRaceTest {
     return observer.events
   }
   
-  func runAutoCompleteSimulation(cutOff: TestTime, fastSpots: Int, _ inputs: [([TKAutocompletionResult], at: TestTime)]) -> [Recorded<Event<[TKAutocompletionResult]>>] {
-    
-    let bag = DisposeBag()
-    let scheduler = TestScheduler(initialClock: 0)
-    let observer = scheduler.createObserver([TKAutocompletionResult].self)
-    
-    let inputEvents: [[Recorded<Event<[TKAutocompletionResult]>>]] = inputs.map { input in
-      if input.0.isEmpty {
-        return [
-          .error(input.at, InputError())
-        ]
-      } else {
-        return [
-          .next(input.at, input.0),
-          .completed(input.at)
-        ]
-      }
-    }
-
-    SharingScheduler.mock(scheduler: scheduler) {
-      let observables = inputEvents
-        .map(scheduler.createHotObservable)
-        .map { $0.asObservable() }
-              
-      let processed = Observable.stableRace(observables, cutOff: .seconds(cutOff), fastSpots: fastSpots, comparer: { $0.score > $1.score }, threshold: 4, getTitle: { $0.title })
-              
-      processed
-        .bind(to: observer)
-        .disposed(by: bag)
-      scheduler.start()
-      scheduler.stop()
-    }
-
-    return observer.events
-    }
 }
-
-
