cd8bb998462518acac2f1dc641fe547a8d55fb4f
Adrian Schoenig <adrian@schoenig.me>
2024-09-02 13:19:51 +1000

    Require iOS 15 for SPM usage

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
   name: "TripKit",
   defaultLocalization: "en",
   platforms: [
-    .iOS(.v13),
+    .iOS(.v15),
     .macOS(.v11),
   ],
   products: [
